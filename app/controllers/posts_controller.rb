bd4839d5aa296bd4ce5d9bb9d4f9a5800b001406
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-06-03 17:57:24 +0000

    cont'd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,19 @@ class PostsController < ApplicationController
 
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = @current_user["id"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
+  else
+    flash["notice"] = "Login first."
   end
+  redirect_to "/posts"
 
 end
